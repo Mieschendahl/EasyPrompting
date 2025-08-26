df38d3a76b2f6bdcacbe8152bfc9ee56c565d289
Mieschendahl <mieschendahl.leonardo@gmail.com>
2025-08-26 16:51:37 +0200

    updated pyproject.toml

--- easy_prompting/__init__.py
+++ easy_prompting/__init__.py
@@ -2,4 +2,3 @@ from easy_prompting._message import Message
 from easy_prompting._llm import LLM, LLMError
 from easy_prompting._option import Option
 from easy_prompting._prompter import Prompter, ChoiceError
-from easy_prompting import prebuilt
